14b03c070f7404a47d5753a5c0bebaf6c85a4bf0
cheon-eunsong <freesky111@naver.com>
2017-10-09 04:54:31 +0900

    Youtube API video (recyclerview)

---
+++ app/src/main/java/com/es/youtubeapi_test/Adapter.java
@@ -0,0 +1,121 @@
+package com.es.youtubeapi_test;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    private Context context;
+    private ArrayList<String> urlList = new ArrayList<>();
+    private Main2Activity mActivity;
+
+    Adapter(Context context, Main2Activity mActivity) {
+        this.context = context;
+        this.mActivity = mActivity;
+    }
+    void addAll(ArrayList<String> urlList){
+        this.urlList = urlList;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ItemViewHolder(LayoutInflater.from(context).inflate(R.layout.item_recyclerview, parent, false),context);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final ItemViewHolder cholder = (ItemViewHolder)holder;
+        final String url = urlList.get(position);
+
+
+        cholder.youTubeThumbnailView.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubeThumbnailView.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, final YouTubeThumbnailLoader youTubeThumbnailLoader) {
+                youTubeThumbnailLoader.setVideo(url);
+                youTubeThumbnailLoader.setOnThumbnailLoadedListener(new YouTubeThumbnailLoader.OnThumbnailLoadedListener() {
+                    @Override
+                    public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
+                        youTubeThumbnailLoader.release();
+//                        youTubeThumbnailView.setVisibility(View.VISIBLE);
+//                        cholder.relativeLayoutOverYouTubeThumbnailView.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader.ErrorReason errorReason) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+                if (youTubeInitializationResult.isUserRecoverableError()){
+                    youTubeInitializationResult.getErrorDialog(mActivity,RECOVERY_DIALOG_REQUEST);
+                }else{
+                    String errorMessage = String.format(context.getString(R.string.error_player),youTubeInitializationResult.toString());
+                    Toast.makeText(context, ""+errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+//        cholder.frameLayout
+//        Toast.makeText(context, "fragment 조각안에 재생", Toast.LENGTH_SHORT).show();
+//        ViewFragment f = ViewFragment.newInstance(url);
+//        FragmentManager manager = mActivity.getSupportFragmentManager();
+//        manager.beginTransaction().replace(R.id.view_container,f).commit();
+    }
+
+    @Override
+    public int getItemCount() {
+        return urlList.size();
+    }
+
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        FrameLayout frameLayout;
+        protected FrameLayout relativeLayoutOverYouTubeThumbnailView;
+        protected ImageView playButton;
+        Context context;
+
+        YouTubeThumbnailView youTubeThumbnailView;
+        ItemViewHolder(View itemView,Context context) {
+            super(itemView);
+            this.context = context;
+            youTubeThumbnailView = (YouTubeThumbnailView)itemView.findViewById(R.id.view_container);
+            playButton=(ImageView)itemView.findViewById(R.id.btnYoutube_player);
+            playButton.setOnClickListener(this);
+            relativeLayoutOverYouTubeThumbnailView = (FrameLayout) itemView.findViewById(R.id.relativeLayout_over_youtube_thumbnail);
+        }
+
+        @Override
+        public void onClick(View view) {
+//            ViewFragment f = ViewFragment.newInstance(urlList.get(getAdapterPosition()));
+//            FragmentManager manager = mActivity.getSupportFragmentManager();
+//            manager.beginTransaction().replace(R.id.relativeLayout_over_youtube_thumbnail,f).commit();
+            Intent intent = YouTubeStandalonePlayer.createVideoIntent(mActivity, urlList.get(getAdapterPosition()), urlList.get(getAdapterPosition()));
+            mActivity.startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/es/youtubeapi_test/Main2Activity.java
+++ app/src/main/java/com/es/youtubeapi_test/Main2Activity.java
@@ -3,19 +3,42 @@ import android.support.v4.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Main2Activity extends AppCompatActivity {
+
+    private Adapter adapter;
+    private RecyclerView recyclerView;
+
+    private ArrayList<String> urlList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        String url = "i_yLpCLMaKk";
+        /*String url = "i_yLpCLMaKk";
         Toast.makeText(this, "fragment 조각안에 재생", Toast.LENGTH_SHORT).show();
         ViewFragment f = ViewFragment.newInstance(url);
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().replace(R.id.view_container,f).commit();
+
+*/
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);// 선형
+        linearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        urlList.add("i_yLpCLMaKk");
+        urlList.add("UEGpGjF7GbA");
+        adapter = new Adapter(this,this);
+        adapter.addAll(urlList);
+        recyclerView.setAdapter(adapter);
+
+
 
         //in fragment
 //        ViewFragment f = ViewFragment.newInstance(url);
